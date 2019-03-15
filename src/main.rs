ab753959f09cda26928e84a9c0e76bb0cfd71aea
leo-lb <lle-bout@zaclys.net>
2019-03-15 23:49:48 +0100

    Rewrite to use futures

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,124 @@
+use futures::{stream, Future, Stream};
+use hyper::{Body, Client, Request, Uri};
+use hyper_tls::HttpsConnector;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use std::iter;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering;
+use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use tokio;
 
 const BASE_URL: &str = "https://i.imgur.com/";
 
-fn generate_link() -> (String, String) {
-    let mut rng = thread_rng();
-    let chars = iter::repeat(())
-        .map(|()| rng.sample(Alphanumeric))
-        .take(7)
-        .collect::<String>();
-    (
-        format!("{}{}.png", BASE_URL, chars),
-        format!("{}.png", chars),
-    )
+struct UriGenerator {
+    base_url: String,
+    extension: String,
 }
 
-fn send_to_discord_webhook(id: u64, token: &String, image_url: &String) {
-    use serenity::http;
-    use serenity::model::channel::Embed;
+impl UriGenerator {
+    fn new(base_url: String, extension: String) -> Self {
+        Self {
+            base_url,
+            extension,
+        }
+    }
+}
 
-    let webhook = http::get_webhook_with_token(id, token).expect("valid webhook");
+impl Iterator for UriGenerator {
+    type Item = Uri;
 
-    let resources = Embed::fake(|e| e.image(image_url));
-
-    let _ = webhook.execute(false, |w| w.embeds(vec![resources]));
+    fn next(&mut self) -> Option<Uri> {
+        Some(
+            format!(
+                "{}{}{}",
+                self.base_url,
+                iter::repeat(())
+                    .map(|()| thread_rng().sample(Alphanumeric))
+                    .take(7)
+                    .collect::<String>(),
+                self.extension
+            )
+            .parse()
+            .unwrap(),
+        )
+    }
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    let n_threads = args.get(1).unwrap().parse().expect("valid thread count");
-    let webhook_id = args
-        .get(2)
-        .unwrap()
-        .parse::<u64>()
-        .expect("valid webhook id");
-    let webhook_token = args.get(3).unwrap();
+    let n_concurrent: usize = args.get(1).unwrap().parse().unwrap();
+    let id: u64 = args.get(2).unwrap().parse().unwrap();
+    let token: String = args.get(3).unwrap().parse().unwrap();
 
-    let pool = threadpool::ThreadPool::new(n_threads);
-    let client = reqwest::Client::builder().gzip(true).build().unwrap();
+    let (tx, rx) = mpsc::channel::<Uri>();
+
+    thread::spawn(move || {
+        use serenity::http;
+        use serenity::model::channel::Embed;
+
+        let webhook = http::get_webhook_with_token(id, &token).expect("valid webhook");
+
+        for uri in rx {
+            let uri_str = format!(
+                "{}{}",
+                uri.authority_part().unwrap(),
+                uri.path_and_query().unwrap()
+            );
+
+            println!("found valid image at {}", uri_str);
+
+            let resources = Embed::fake(|e| e.image(uri_str));
+
+            let _ = webhook.execute(false, |w| w.embeds(vec![resources]));
+        }
+    });
 
     let images_per_second = Arc::new(AtomicUsize::new(0));
 
     {
         let images_per_second = images_per_second.clone();
-
-        pool.execute(move || loop {
+        thread::spawn(move || loop {
             println!("{} images / s", images_per_second.load(Ordering::SeqCst));
             images_per_second.store(10, Ordering::SeqCst);
             thread::sleep(Duration::from_secs(1));
         });
     }
 
-    // + 1 for statistics thread
-    for _ in 0..n_threads + 1 {
-        let client = client.clone();
-        let images_per_second = images_per_second.clone();
-        let webhook_id = webhook_id.clone();
-        let webhook_token = webhook_token.clone();
+    let https = HttpsConnector::new(4).expect("TLS initialization failed");
+    let client = Client::builder().build::<_, hyper::Body>(https);
 
-        pool.execute(move || loop {
-            let (link, _path) = generate_link();
+    let images = UriGenerator::new(BASE_URL.to_string(), ".png".to_string());
 
-            // println!("fetching {}", link);
-            match client.head(&link).send() {
-                Ok(resp) => {
-                    images_per_second.fetch_add(1, Ordering::SeqCst);
-                    // println!("received headers for {}", link);
-                    match resp.content_length() {
-                        Some(len) => {
-                            // println!("Content-Length for {} is {}", link, len);
-                            if len > 1000 {
-                                /* match std::fs::File::create(&path) {
-                                    Ok(mut file) => {
-                                        println!("created file {}, now writing..", &path);
-                                        resp.copy_to(&mut file).is_err();
-                                    }
-                                    Err(e) => {
-                                        eprintln!("{}", e);
-                                    }
-                                }; */
+    let work = stream::iter_ok(images)
+        .map(move |uri| {
+            client
+                .request(Request::head(uri.clone()).body(Body::empty()).unwrap())
+                .map(move |res| (res, uri))
+        })
+        .buffer_unordered(n_concurrent)
+        .and_then(move |(res, uri)| {
+            images_per_second.fetch_add(1, Ordering::SeqCst);
 
-                                println!("found valid image at {}", &link);
-
-                                send_to_discord_webhook(webhook_id, &webhook_token, &link);
-                            }
-                        }
-                        None => {
-                            eprintln!("Content-Length absent");
+            if let Some(content_length) = res.headers().get("content-length") {
+                if let Ok(content_length) = content_length.to_str() {
+                    if let Ok(content_length) = content_length.parse::<usize>() {
+                        if content_length > 503 {
+                            tx.send(uri).unwrap();
                         }
                     }
                 }
-                Err(e) => {
-                    eprintln!("{}", e);
-                }
-            };
+            }
+            res.into_body().concat2()
+        })
+        .for_each(|_body| Ok(()))
+        .map_err(|e| {
+            eprintln!("{}", e);
         });
-    }
 
-    pool.join();
+    tokio::run(work);
 }
